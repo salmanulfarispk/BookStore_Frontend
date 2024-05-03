26fe1c0108934244bead5ce109af34fc649d19f2
Salmanul FarisPK <salmanulfarispk1405@gmail.com>
2024-05-03 15:50:33 +0530

    finished

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import Courses from "./components/Courses/Courses"
+import Home from "./components/Home/Home"
+import  { Routes,Route, Navigate } from "react-router-dom"
+import Signup from "./components/Signup"
+import { Toaster } from "react-hot-toast";
+import { useAuth } from "./context/AuthProvider";
+
+
+function App() {
+  const [authUser, setAuthUser] = useAuth();
+
+  return (
+    <>
+  <div className="dark:bg-slate-900 dark:text-white">
+        <Routes>
+          <Route path="/" element={<Home />} />
+           <Route
+            path="/course"
+            element={authUser ? <Courses /> : <Navigate to="/signup" />}
+          />
+          
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+        <Toaster />
+        
+      </div>
+
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Courses/Courses.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Navbar from '../Navbar'
+import Course from '../Course'
+import Footer from '../Footer'
+
+
+
+
+export default function Courses() {
+  return (
+    <>
+    <Navbar/>
+    <div className='min-h- dark:bg-slate-900 dark:text-white'>
+    <Course/>
+    </div>
+     <Footer/>
+    
+    
+    
+    </>
+  )
+}
