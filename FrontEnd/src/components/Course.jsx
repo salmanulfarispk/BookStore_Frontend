3898ede6651ebc73ab679e0d41068f47c16901bd
Salmanul FarisPK <salmanulfarispk1405@gmail.com>
2024-05-22 22:04:16 +0530

    minor changes

--- FrontEnd/src/components/Course.jsx
+++ FrontEnd/src/components/Course.jsx
@@ -15,23 +15,25 @@ export default function Course() {
       } catch (error) {
         console.log(error);
       }
-    };
+    };   
     getBook();
+    
   }, []);
+
   return (
     <>
     <div className=" max-w-screen-2xl container mx-auto md:px-20 px-4 ">
       <div className="mt-28 items-center justify-center text-center">
         <h1 className="text-2xl  md:text-4xl">
           We're delighted to have you{" "}
-          <span className="text-pink-500"> Here! :)</span>
+          <span className="text-pink-500"> Here! :) </span>   
         </h1>
         <p className="mt-12">
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro,
           assumenda? Repellendus, iste corrupti? Tempore laudantium
           repellendus accusamus accusantium sed architecto odio, nisi expedita
           quas quidem nesciunt debitis dolore non aspernatur praesentium
-          assumenda sint quibusdam, perspiciatis, explicabo sequi fugiat amet
+          assumenda sint quibusdam, perspiciati explicabo sequi fugiat amet
           animi eos aut. Nobis quisquam reiciendis sunt quis sed magnam
           consequatur!
         </p>
